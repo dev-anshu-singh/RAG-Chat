d30ff745ef4a4272321c284108458aa75a3a5279
dev-anshu-singh <devanshu.230103012@iiitbh.ac.in>
2026-05-17 13:07:18 +0530

    schemas, route

---
+++ app/api/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from datetime import datetime
+from app.db.database import ProcessingStatus
+
+class QueryRequest(BaseModel):
+    question: str
+    # start a new chat or continue an old one by passing the same ID.
+    thread_id: Optional[str] = None
+
+
+class QueryResponse(BaseModel):
+    answer: str
+    thread_id: str
+
+class DocumentResponse(BaseModel):
+    id: int
+    filename: str
+    upload_date: datetime
+    status: ProcessingStatus
+    chunk_count: int
+    error_message: Optional[str] = None
+
+    class Config:
+        from_attributes = True
